c281b99b2786b810246694d362a60da965dcea16
nicolascommegna1@gmail.com <nikola1995>
2018-06-25 15:26:40 +0200

    Log In with Facebook and Google added

---
+++ app/src/main/java/it/uniba/di/nitwx/progettoMobile/LogIn.java
@@ -0,0 +1,96 @@
+package it.uniba.di.nitwx.progettoMobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.LoginResult;
+import com.facebook.*;
+
+import java.util.Arrays;
+import com.google.android.gms.auth.api.signin.*;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
+
+
+public class LogIn extends AppCompatActivity {
+    CallbackManager callbackManager;
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
+    GoogleSignInClient  mGoogleSignInClient;
+    TextView email;
+    private static final int RC_SIGN_IN = 9001;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            GoogleSignInAccount user=task.getResult();
+            String userEmail=user.getEmail();
+            email.setText(userEmail);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        email = (TextView) findViewById(R.id.testMail);
+        //sign in google
+        mGoogleSignInClient= GoogleSignIn.getClient(this, gso);
+        SignInButton googleBtn = (SignInButton) findViewById(R.id.sign_in_button);
+        googleBtn.setOnClickListener(googleSignIn);
+        //facebook sign in
+         callbackManager = CallbackManager.Factory.create();
+        //get info about user.app interaction.
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        LoginButton btn = (LoginButton) findViewById(R.id.btnLogIn);
+        btn.setReadPermissions( Arrays.asList("public_profile"));
+
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        AccessToken accessToken = loginResult.getAccessToken();
+                        if (accessToken != null) {
+                            Toast.makeText(LogIn.this, "successo", Toast.LENGTH_LONG);
+                        }
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+    public View.OnClickListener googleSignIn = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            signIn();
+        }
+    };
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+}
