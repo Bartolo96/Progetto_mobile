4f8596045c74a06e49df84a430f4d2e5df40a96b
antoanto27 <30901970+antoanto27@users.noreply.github.com>
2018-07-04 11:45:43 +0200

    Da sistemare Profile

--- app/src/main/java/it/uniba/di/nitwx/progettoMobile/ProfileActivity.java
+++ app/src/main/java/it/uniba/di/nitwx/progettoMobile/ProfileActivity.java
@@ -1,11 +1,7 @@
 package it.uniba.di.nitwx.progettoMobile;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,8 +9,5 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 }
