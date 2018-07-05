99ca67fbfe1fb63dae25a78913f03a024611f1ca
antoanto27 <30901970+antoanto27@users.noreply.github.com>
2018-07-05 15:54:59 +0200

    .

--- app/src/main/java/it/uniba/di/nitwx/progettoMobile/ProfileActivity.java
+++ app/src/main/java/it/uniba/di/nitwx/progettoMobile/ProfileActivity.java
@@ -1,7 +1,11 @@
 package it.uniba.di.nitwx.progettoMobile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,16 +15,40 @@ import io.jsonwebtoken.Jwts;
 
 public class ProfileActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         ImageView profileImage = (ImageView) findViewById(R.id.profileImageView);
+        profileImage.setOnClickListener(loadImageListener);
         TextView nameProfile = (TextView) findViewById(R.id.nameProfileTextView);
         TextView emailProfile = (TextView) findViewById(R.id.emailProfileTextView);
         TextView bDayProfile = (TextView) findViewById(R.id.bDayProfileTextView);
 
+    }
 
+    View.OnClickListener loadImageListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), Constants.SELECT_PICTURE_REQUEST);
+        }
+    };
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == Constants.SELECT_PICTURE_REQUEST) {
+                // Get the url from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // Set the image in ImageView
+                    ((ImageView) findViewById(R.id.profileImageView)).setImageURI(selectedImageUri);
+                }
+            }
+        }
     }
 }
