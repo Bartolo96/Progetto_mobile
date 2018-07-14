7db951b6dcf9b8616783b28fe8104845302eae8e
antoanto27 <30901970+antoanto27@users.noreply.github.com>
2018-07-14 20:35:50 +0200

    ..

--- app/src/main/java/it/uniba/di/nitwx/progettoMobile/HelpActivity.java
+++ app/src/main/java/it/uniba/di/nitwx/progettoMobile/HelpActivity.java
@@ -1,7 +1,12 @@
 package it.uniba.di.nitwx.progettoMobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle(getResources().getString(R.string.help));
+
+        Button sendForm = findViewById(R.id.sendFormHelpActivity);
+        sendForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HelpActivity.this,getResources().getString(R.string.tyForSend),Toast.LENGTH_SHORT).show();
+                navigateUpTo(new Intent(HelpActivity.this,HomeActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+
+            navigateUpTo(new Intent(this,HomeActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
